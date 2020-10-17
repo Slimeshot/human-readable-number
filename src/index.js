e04ed63fd0685907e8be700c4dc9f768bbff6d5b
Evgeny Hohlov <slimeshot@mail.ru>
2020-10-17 13:03:16 +0300

    feat: add file with completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function toReadable (number) {
-  
+    let result = '';
+    const stringNumber = ['zero', 'one', 'two', 'three', 'four', 'five',
+    'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen',
+    'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    
+    const stringNumberTen = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number < 20) {
+        return stringNumber[number]
+    } else if (number >= 20 && number <= 99) {
+        if (number % 10 == 0) {
+            return(`${stringNumberTen[number / 10]}`)
+        } else {
+            return(`${stringNumberTen[Math.floor(number / 10)]} ${stringNumber[number % 10]}`)
+        }
+    
+    } else if (number >= 100 && number <= 999) {
+        let char = number % 100;
+        if (number % 100 == 0) {
+            return (`${stringNumber[number / 100]} hundred`);
+        } else {
+            if (number % 10 == 0) {
+                return `${stringNumber[Math.floor(number / 100)]} hundred ${stringNumberTen[(number / 10) % 10]}`;
+            } else {
+                if (char < 20) {
+                    return `${stringNumber[Math.floor(number / 100)]} hundred ${stringNumber[number % 100]}`
+
+                } else {
+                    return `${stringNumber[Math.floor(number / 100)]} hundred ${stringNumberTen[(Math.floor(number / 10)) % 10]} ${stringNumber[number % 10]}`
+                }
+            }
+        }
+    }
 }
